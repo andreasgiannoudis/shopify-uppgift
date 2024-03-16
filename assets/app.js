8dc2dd2b844417a4b19c3f47cbf763c23f41cec9
andreasgiannoudis <agiannoudis@hotmail.com>
2024-03-16 18:22:04 +0100

    Removed the copyright section from footer and added the correct logos.

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,19 @@
-
+function updateShippingOptions() {
+  const selectedShippingMethod = document.querySelector('input[name="shipping_methods"]:checked');
+  if (selectedShippingMethod) {
+    const shippingMethodId = selectedShippingMethod.id;
+    if (shippingMethodId.includes("Free_Shipping")) {
+      document.querySelectorAll(".B4zH6:not(.HKtYc)").forEach((option) => {
+        option.style.display = "none";
+      });
+    } else {
+      document.querySelectorAll(".B4zH6").forEach((option) => {
+        option.style.display = "block";
+      });
+    }
+  }
+}
+document.querySelectorAll('input[name="shipping_methods"]').forEach((radio) => {
+  radio.addEventListener("change", updateShippingOptions);
+});
+updateShippingOptions();
--- resources/js/free-shipping.js
+++ resources/js/free-shipping.js
@@ -1,26 +1,29 @@
+// Function to update shipping options based on selected shipping method
 function updateShippingOptions() {
-    //alert('before selected shipping method');
     const selectedShippingMethod = document.querySelector('input[name="shipping_methods"]:checked');
-    //alert(selectedShippingMethod);
+    console.log(selectedShippingMethod);
     if (selectedShippingMethod) {
         const shippingMethodId = selectedShippingMethod.id;
 
-
+        // Check if the selected shipping method ID contains "Free_Shipping"
         if (shippingMethodId.includes('Free_Shipping')) {
-
+            // If free shipping is selected, hide non-free shipping options
             document.querySelectorAll('.B4zH6:not(.HKtYc)').forEach(option => {
-                option.style.display = 'none';
+                option.style.display = 'none'; // Hide non-free shipping options
             });
         } else {
+            // If another shipping option is selected, show all options
             document.querySelectorAll('.B4zH6').forEach(option => {
-                option.style.display = 'block'; 
+                option.style.display = 'block'; // Show all shipping options
             });
         }
     }
 }
 
+// Listen for changes in the selected shipping method
 document.querySelectorAll('input[name="shipping_methods"]').forEach(radio => {
     radio.addEventListener('change', updateShippingOptions);
 });
 
+// Call the function initially to set the initial state
 updateShippingOptions();
